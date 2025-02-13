1c0a725ddeb6b70798e38de2373df03760507a28
jcastellanog <jesus.castellano@hotmail.com>
2025-02-13 14:14:19 +0100

    Se ha agregado la estructura del programa

--- PruebaCV/Program.cs
+++ PruebaCV/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // VARIABLES
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
         }
     }
 }
