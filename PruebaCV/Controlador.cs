0419d0665110baae4028550699cbd69ab34be214
jcastellanog <jesus.castellano@hotmail.com>
2025-02-19 08:45:39 +0100

    Estructra Organizativa del proyecto: Clases Instanciables y Estáticas

---
+++ PruebaCV/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV
+{
+    /// <summary>
+    /// Clase estática que sirve de intermediara entre el Modelo y las Vistas de la App
+    /// </summary>
+    public static class Controlador
+    {
+    }
+}
---
+++ PruebaCV/Vehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCV
+{
+    /// <summary>
+    /// Clase Instanciable - Se pueden generar objetos que se alojarán en memoria
+    /// </summary>
+    public class Vehiculo
+    {
+    }
+}
