040a5d06d1c28b7a555b3b6ce57629d71054621c
jcastellanog <jesus.castellano@hotmail.com>
2025-02-19 09:07:43 +0100

    Establecimiento de la Estructura del Modelo de Datos (Clase Instanciable)

--- PruebaCV/Vehiculo.cs
+++ PruebaCV/Vehiculo.cs
@@ -11,5 +11,26 @@ namespace PruebaCV
     /// </summary>
     public class Vehiculo
     {
+        #region CONSTANTES - PARÁMETROS DE CONFIGURACIÓN DE LA CLASE
+        #endregion
+
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+        #endregion
+
+        #region CONSTRUCTORES
+        #endregion
+
+        // Mecanismo de acceso a la información que mantiene la clase (datos)
+        // Proporciona dos Descriptores de Acceso (get y el set)
+        // Su misión es la de proteger los datos que entran a la clase (set)
+        // o se transfieren fuera de la clase (get)
+        #region PROPIEDADES
+        #endregion
+
+        #region MÉTODOS PRIVADOS - INTERNOS DE LA CLASE
+        #endregion
+
+        #region MÉTODOS PÚBLICOS - ACCESIBLES DESDE FUERA DE LA CLASE
+        #endregion
     }
 }
